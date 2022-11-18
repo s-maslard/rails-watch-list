f6a39164d3d2d50b049010162bb776acc884e139
sylvain maslard <sylv5@hotmail.fr>
2022-11-18 14:47:33 +0100

    Watch List

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,25 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: %i[new create]
 
   def new
-
+    @bookmark = Bookmark.new
   end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to bookmark_path(@bookmark)
+    else
+      render :new
+    end
+  end
+end
+
+private
+def set_list
+  @bookmark = Bookmark.find(params[:id])
+end
+
+def bookmark_params
+  params.require(:bookmark).permit(:comment)
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,29 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: %i[show]
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = list.new
+  end
+
+  def create
+    @list = list.new
+    @list.save
+    redirect_to list_path(@list)
+  end
 end
+
+  private
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
